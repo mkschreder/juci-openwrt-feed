9c7f80dd8ebd2a0ee3ce5324d55c67ce44d7b06e
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-02 22:46:38 +0200

    update travis to cache openwrt directory

--- build-uml-image.sh
+++ build-uml-image.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-git clone http://git.openwrt.org/15.05/openwrt.git openwrt
+if [ -d openwrt ]; then 
+	echo "WARNING: not deleting existing working directory."; 
+else
+	git clone http://git.openwrt.org/15.05/openwrt.git openwrt
+fi
 cp openwrt-bootstrap.sh openwrt
 cd openwrt 
 
 ./openwrt-bootstrap.sh
 make V=s
 cp bin/uml/openwrt-uml-vmlinux bin/uml/openwrt-uml-ext4.img ./
+tar -czf openwrt-juci-uml.tar.gz openwrt-uml*
 
 echo "Build completed!"
 echo "You can now run your uml target here using: "

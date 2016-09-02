8ab509edd6651d82839463afde1dbae451cb448a
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-03 00:23:03 +0200

    update travis build scripts

--- build-uml-image.sh
+++ build-uml-image.sh
@@ -23,6 +23,6 @@ tar -czf openwrt-juci-uml.tar.gz openwrt-uml*
 
 echo "Build completed!"
 echo "You can now run your uml target here using: "
-echo "   ./openwrt-uml-vmlinux ubd0=$PWD/openwrt-uml-ext4.img eth0=tuntap,tap0"
+echo "   ./openwrt/openwrt-uml-vmlinux ubd0=$PWD/openwrt/openwrt-uml-ext4.img eth0=tuntap,tap0"
 echo "Note: for network access you need to actually set up tuntap device on your host system!"
 
